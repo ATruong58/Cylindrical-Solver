b34edd712125f8a94084d798a2fc3a81464b1070
Alan <atwpb@mst.edu>
2019-02-14 17:28:40 -0600

    main now read in text file when user passed it on the command line

--- driver.cpp
+++ driver.cpp
@@ -1,38 +1,50 @@
 #include "cylinder.h"
 #include "norm.h"
+#include <fstream>
 
-int main(){
+int main(int argc,  char * argv[])
+{
     std::vector<cylindricalNum<double>> set_of_coordinate;
     cylindricalNum<double> temp;
     norm driver;
     int loop;
-    
-    std::cin >> loop;
     try
     {
-
-        if(loop < 5)
+        if(argc > 1)
         {
-            throw "Too little amount of dataset";
-        }
+            std::ifstream fs(argv[1]);
+            if (!(fs.is_open() && fs.good()))
+            {
+                throw "File not read correctly\n";
+            }
+            fs >> loop;
+            if(loop < 5)
+            {
+                throw "Too little amount of dataset\n";
+            }
 
-        for(int i = 0; i < loop; i++)
+            for(int i = 0; i < loop; i++)
+            {
+                fs >> temp;
+                set_of_coordinate.push_back(temp);
+
+            }
+
+            
+            std::cout << -set_of_coordinate[0] << std::endl;
+            std::cout << !set_of_coordinate[1] << std::endl;
+            std::cout << (set_of_coordinate[3] < set_of_coordinate[4]);
+            std::cout << std::endl << driver(set_of_coordinate) << std::endl;
+        }
+        else
         {
-            std::cin >> temp;
-            set_of_coordinate.push_back(temp);
-
+            throw "No arguments.\n";
         }
-
     }
     catch (std::string message)
     {
         std::cout << message << std::endl;
     }
-            
-    std::cout << -set_of_coordinate[0] << std::endl;
-    std::cout << !set_of_coordinate[1] << std::endl;
-    std::cout << (set_of_coordinate[3] < set_of_coordinate[4]);
-    std::cout << std::endl << driver(set_of_coordinate) << std::endl;
 
     return 0;
 }
